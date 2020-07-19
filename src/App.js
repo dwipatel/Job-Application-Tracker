b9503c34a6f330712e9f24a55eb8d6dcc68ccca7
dwipatel <dwipatel00@gmail.com>
2020-07-19 18:40:22 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import FrontPage from './components/overviewPage';
+import AddPage from './components/addApplication';
+import EditPage from './components/editApplication';
+import Register from './components/auth/register';
+import LoginPage from './components/loginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <Navbar/>
+          <Route path="/" exact component={FrontPage}/>
+          <Route path="/add" component={AddPage}/> 
+          <Route path="/edit/:id" component={EditPage}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/login" component={LoginPage}/>
+        </div>
+      </Router>
+    );
+  }  
 }
-
-export default App;
