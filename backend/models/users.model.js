9bed4cc9ae3abcd664ab064073755a777b0d10cd
dwipatel <dwipatel00@gmail.com>
2020-07-19 00:58:02 -0400

    Created users schema

---
+++ backend/models/users.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+    username: { 
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+    },
+    password_confirmation: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+    }
+}, {
+    timestamps: true,
+})
+
+const User = mongoose.model('User', usersSchema);
+
+module.exports = User;
