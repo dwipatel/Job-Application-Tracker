7af345b66352160412e7d27aae7f8902b7aa539e
dwipatel <dwipatel00@gmail.com>
2020-07-22 16:20:24 -0400

    Added route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -44,9 +44,23 @@ router.route('/login').post( async (req, res) => {
             username: user.username,
         }
     });
-
-    
-    
 })
+
+router.post("/tokenIsValid", async (req, res) => {
+    try {
+      const token = req.header("x-auth-token");
+      if (!token) return res.json(false);
+  
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      if (!verified) return res.json(false);
+  
+      const user = await User.findById(verified.id);
+      if (!user) return res.json(false);
+  
+      return res.json(true);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
 
 module.exports = router;
