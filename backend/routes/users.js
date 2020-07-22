fcf77c5fe3c9631fed88e53326083e5605640c90
dwipatel <dwipatel00@gmail.com>
2020-07-21 23:58:58 -0400

    Updated routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken')
 let User = require('../models/users.model');
 const { model } = require('mongoose');
 
@@ -23,6 +24,29 @@ router.route('/add').post((req, res) => {
     newUser.save()
         .then(() => res.json('User added'))
         .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/login').post( async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await User.findOne({ username: username });
+    if (!user) return res.status(400).json({ msg: "No account with this username registered" })
+
+    const isMatch = (password === user.password) ? true : false;
+    if(!isMatch) return res.status(400).json({ msg: "Invalid credentials"})
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    res.json({
+        token,
+        user: {
+            id: user._id,
+            username: user.username,
+        }
+    });
+
+    
+    
 })
 
 module.exports = router;
