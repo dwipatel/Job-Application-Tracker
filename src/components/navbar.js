cb785e0e2e905a0c7694e97a6e19970b666ec2ac
dwipatel <dwipatel00@gmail.com>
2020-07-24 15:15:30 -0400

    Added login and logout functionality

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Button } from 'react-bootstrap'
+import UserContext from '../context/UserContext';
 
-export default class Navigation extends Component {
-    render() {
-        return (
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/">App Tracker</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Overview</Nav.Link>
-                    <Nav.Link href="/add">Add</Nav.Link>
-                </Nav>
-            </Navbar>
-        )
+export default function Navigation () {
+    const { userData, setUserData } = useContext(UserContext);
+
+    const logout = () => {
+        setUserData({
+            token: undefined,
+            user: undefined,
+        });
+        localStorage.setItem("auth-token", "");
     }
+    
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">App Tracker</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="/">Overview</Nav.Link>
+                <Nav.Link href="/add">Add</Nav.Link>
+            </Nav>
+            { 
+                userData.user ? (
+                    <a><Button onClick={logout} variant="info">Logout</Button></a>
+                ) : (
+                    <div>
+                        <a href="/login" className="mr-2"><Button variant="info">Login</Button></a>
+                        <a href="/register"><Button variant="outline-info">Register</Button></a>
+                    </div>
+                )
+            }
+            
+        </Navbar>
+    )
+    
 }
