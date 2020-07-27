7d4aa45a261df2b5e0d45916717f2ccb49d1795b
dwipatel <dwipatel00@gmail.com>
2020-07-27 14:02:00 -0400

    Added userID value

--- backend/models/applications.model.js
+++ backend/models/applications.model.js
@@ -5,11 +5,12 @@ const applicationSchema = new Schema({
     company: { type: String, required: true },
     title: { type: String, required: true },
     link: { type: String, required: true },
-    date: { type: Date, required: true},
-    notes: { type: String, required: false},
+    date: { type: Date, required: true },
+    notes: { type: String, required: false },
+    userID: { type: String, required: true },
 }, {
     timestamps: true,
-})
+});
 
 const Application = mongoose.model('Application', applicationSchema);
 
